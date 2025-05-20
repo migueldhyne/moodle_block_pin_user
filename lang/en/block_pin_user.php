fda4b82989f24a2bf049caf289ce15136c940ead
migueldhyne <87825608+migueldhyne@users.noreply.github.com>
2025-05-20 10:23:31 +0200

    Add files via upload

---
+++ lang/en/block_pin_user.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Languages configuration for the block_pin_user plugin.
+ *
+ * @package   block_pin_user
+ * @copyright 2025, Miguël Dhyne <miguel.dhyne@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['badge1settings']             = 'Settings for Badge 1';
+$string['badge1settings_desc']        = 'Configure the appearance of the first badge.';
+$string['badge2settings']             = 'Settings for Badge 2';
+$string['badge2settings_desc']        = 'Configure the appearance of the second badge.';
+$string['contains']                   = 'Must contain';
+$string['custombadge1bg']             = 'Background color for Badge 1';
+$string['custombadge1bg_desc']        = 'Set the background color for the first badge.';
+$string['custombadge1color']          = 'Text color for Badge 1';
+$string['custombadge1color_desc']     = 'Set the text color for the first badge.';
+$string['custombadge2bg']             = 'Background color for Badge 2';
+$string['custombadge2bg_desc']        = 'Set the background color for the second badge.';
+$string['custombadge2color']          = 'Text color for Badge 2';
+$string['custombadge2color_desc']     = 'Set the text color for the second badge.';
+$string['equals']                     = 'Must equal';
+$string['isempty']                    = 'Must be empty';
+$string['isnotempty']                 = 'Must not be empty';
+$string['notcontains']                = 'Must not contain';
+$string['pin_user']                   = 'Pin User';
+$string['pin_user:addinstance']       = 'Add a new pin_user block';
+$string['pin_user:myaddinstance']     = 'Add a new pin_user block to the My Moodle page';
+$string['pluginname']                 = 'Pin User';
+$string['pluginnamedisplay']          = 'Pin User';
+$string['profilefield1']              = 'Profile field 1';
+$string['profilefield1_desc']         = 'Shortname of the first custom user profile field.';
+$string['profilefield1condition']     = 'Condition for Profile Field 1';
+$string['profilefield1condition_desc'] = 'Select the condition that Profile Field 1 must meet.';
+$string['profilefield1settings']      = 'Profile Field 1 Settings';
+$string['profilefield1settings_desc'] = 'Configure the behavior for Profile Field 1.';
+$string['profilefield1value']         = 'Value for Profile Field 1';
+$string['profilefield1value_desc']    = 'Enter the value to check for Profile Field 1 when the condition is "equals," "contains," or "does not contain."';
+$string['profilefield2']              = 'Profile field 2';
+$string['profilefield2_desc']         = 'Shortname of the second custom user profile field.';
+$string['profilefield2condition']     = 'Condition for Profile Field 2';
+$string['profilefield2condition_desc'] = 'Select the condition that Profile Field 2 must meet.';
+$string['profilefield2settings']      = 'Profile Field 2 Settings';
+$string['profilefield2settings_desc'] = 'Configure the behavior for Profile Field 2.';
+$string['profilefield2value']         = 'Value for Profile Field 2';
+$string['profilefield2value_desc']    = 'Enter the value to check for Profile Field 2 when the condition is "equals," "contains," or "does not contain."';
+$string['text1']                      = 'Text 1';
+$string['text1_desc']                 = 'Enter the value for Text 1.';
+$string['text2']                      = 'Text 2';
+$string['text2_desc']                 = 'Enter the value for Text 2.';
